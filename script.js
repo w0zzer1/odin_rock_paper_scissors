36d07218ff8bafc55a079c4a509b25cf071926a9
w0zzer1 <48273822+w0zzer1@users.noreply.github.com.>
2023-10-08 18:22:17 +0200

    getComputerChoice implemented

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+        switch (random){
+            case 1:
+                console.log("rock");
+                break;
+            case 2:
+                console.log("paper")
+                break;
+            case 3:
+                console.log("scissors")
+        }
+}
+
+getComputerChoice();
