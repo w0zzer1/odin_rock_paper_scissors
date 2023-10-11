f87b142b8a670d4a086e6a9ca286dc04658b785b
w0zzer1 <48273822+w0zzer1@users.noreply.github.com.>
2023-10-11 14:26:22 +0200

    Buttons added to play the game

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let wins = 0;
+let loses = 0;
+
+let btn = document.querySelectorAll('button');
+let plyerScoreDisplay = document.querySelector(".playerScore");
+let computerScoreDisplay = document.querySelector(".computerScore");
+let resultDisplay = document.querySelector(".result");
+let roundDisplay = document.querySelector(".round");
+
 //Generate random choice of the computer
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
@@ -43,27 +52,77 @@ function playRound(computerSelection, playerSelection) {
     }
 }
 
-function game() {
-    let wins = 0;
-    let loses = 0;
-    for (let i= 0; i<5; i++){
-        let playerSelection = prompt("Choose one:");
-        let computerSelection = getComputerChoice();
-        let result = playRound(computerSelection, playerSelection);
-        console.log(result);
-        if (result.includes("You Win!")){
-            wins = wins + 1;
-        } else if (result.includes("You Lost")){
-            loses = loses + 1;
-        }
+
+
+btn.forEach(function (i) {
+    i.addEventListener('click', ()=> {
+        // console.log(i.value);
+        playerSelection = i.value;
+        game();
+    });
+});
+
+function game(){
+    let result = playRound(getComputerChoice(), playerSelection);
+    if (result.includes("You Win!")){
+        wins = wins + 1;
+    } else if (result.includes("You Lost")){
+        loses = loses + 1;
     }
-    if (wins > loses) {
-        console.log("You won overall"); 
-    } else if(loses > wins){
-        console.log("You lost overall");
-    } else {
-        console.log("Overall result tie");
-    }
+    updateDisplay(result);
+    checkEnd();
 }
 
-game();
+function updateDisplay(result){
+    computerScoreDisplay.textContent = String(loses);
+    plyerScoreDisplay.textContent = String(wins);
+    resultDisplay.textContent = String(result);
+}
+
+function checkEnd() {
+    if (wins === 5){
+        result = "Player Won overall";
+        wins=0;
+        loses = 0;
+        updateDisplay(result);
+    } else if (loses === 5){
+        result = "Computer Won overall";
+        wins = 0;
+        loses = 0;
+        updateDisplay(result);
+    }
+}
+// function game() {
+//     let win = 0;
+//     let lose = 0;
+//     let computerSelection= getComputerChoice();
+//     let result = playRound(computerSelection, playerSelection);
+//     if (result.includes("You Win!")){
+//         wins = wins + 1;
+//     } else if (result.includes("You Lost")){
+//         lose = lose + 1;
+//     }
+// }
+// function game() {
+//     let wins = 0;
+//     let loses = 0;
+//     for (let i= 0; i<5; i++){
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(computerSelection, playerSelection);
+//         console.log(result);
+//         if (result.includes("You Win!")){
+//             wins = wins + 1;
+//         } else if (result.includes("You Lost")){
+//             loses = loses + 1;
+//         }
+//     }
+//     if (wins > loses) {
+//         console.log("You won overall"); 
+//     } else if(loses > wins){
+//         console.log("You lost overall");
+//     } else {
+//         console.log("Overall result tie");
+//     }
+// }
+
+// game();
